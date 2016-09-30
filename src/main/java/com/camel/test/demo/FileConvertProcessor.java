bf45e9112132ddc2064194524c15bcf085521770
zhaoyulong <1307232659@qq.com>
2016-09-30 17:22:00 +0800

    额外加了camel的测试代码！

---
+++ src/main/java/com/camel/test/demo/FileConvertProcessor.java
@@ -0,0 +1,40 @@
+package com.camel.test.demo;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+/**
+ *
+ * Created by keen.zhao on 2016/9/30.
+ */
+public class FileConvertProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        try {
+
+            InputStream body = exchange.getIn().getBody(InputStream.class);
+            BufferedReader in = new BufferedReader(new InputStreamReader(body, "UTF-8"));
+            StringBuilder sb = new StringBuilder("");
+            String str;
+            str = in.readLine();
+            while (str != null) {
+                System.out.println(str);
+                sb.append(str);
+                sb.append(" ");
+                str = in.readLine();
+            }
+            exchange.getOut().setHeader(Exchange.FILE_NAME, "converted" + new Random().nextInt(100) + ".txt");
+            //设置输出到文件
+            exchange.getOut().setBody(sb.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/camel/test/demo/FileProcessWithCamel.java
+++ src/main/java/com/camel/test/demo/FileProcessWithCamel.java
@@ -1,16 +1,9 @@
 package com.camel.test.demo;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Random;
 
 
 /**
@@ -26,36 +19,8 @@ public class FileProcessWithCamel {
         context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-
-                Processor processor = new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        try {
-
-                            InputStream body = exchange.getIn().getBody(InputStream.class);
-                            BufferedReader in = new BufferedReader(new InputStreamReader(body,"UTF-8"));
-                            StringBuilder sb = new StringBuilder("");
-                            String str;
-                            str = in.readLine();
-                            while (str != null) {
-                                System.out.println(str);
-                                sb.append(str);
-                                sb.append(" ");
-                                str = in.readLine();
-                            }
-                            exchange.getOut().setHeader(Exchange.FILE_NAME, "converted"+ new Random().nextInt(100)+".txt");
-                            //设置输出到文件
-                            exchange.getOut().setBody(sb.toString());
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                };
-
+                Processor processor =new FileConvertProcessor();
                 from("file:d:/temp/inbox?noop=true&delay=10000").process(processor).to("file:d:/temp/outbox");
-
             }
         });
 
