0f3f2ed7653afc623341220485f7e18b2b5f834a
zhaoyulong <1307232659@qq.com>
2016-09-30 14:02:20 +0800

    额外加了camel的测试代码！

--- src/main/java/com/camel/test/demo/FileMoveWithCamel.java
+++ src/main/java/com/camel/test/demo/FileMoveWithCamel.java
@@ -1,5 +1,11 @@
 package com.camel.test.demo;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Apache Camel是Apache基金会下的一个开源项目,它是一个基于规则路由和处理的引擎，
  * 提供企业集成模式的Java对象的实现，通过应用程序接口或称为陈述式的Java领域特定语言(DSL)
@@ -17,11 +23,35 @@ package com.camel.test.demo;
  * 例子体现了一个最简单的路由功能,比如d:/temp/inbox/是某一个系统FTP到Camel所在的系统的一个接收目录。
  * d:/temp/outbox为Camel要发送的另一个系统的接收目录。
  * <p>
+ * 运行后完成的工作是将d:/temp/inbox/下的所有文件移到d:/temp/outbox
+ * <p>
  * Created by keen.zhao on 2016/9/30.
  */
 public class FileMoveWithCamel {
 
-    public static void main(String[] args) {
+    static Logger log= LoggerFactory.getLogger(FileMoveWithCamel.class);
+
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(
+                new RouteBuilder() {
+                    @Override
+                    public void configure() throws Exception {
+                        //delay=30000是每隔30秒轮询一次文件夹中是否有文件
+                        from("file:d:/temp/inbox?delay=10000").to("file:d:/temp/outbox");
+                    }
+                }
+        );
+
+        context.start();
+        boolean loop = true;
+
+        while (loop) {
+            Thread.sleep(25000);
+            log.info("测试运行中......");
+        }
+
+        context.stop();
 
     }
 
