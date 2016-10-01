30d091d53ef61967d9c7a5890b97401a1e62e93e
zhaoyulong <1307232659@qq.com>
2016-10-02 03:25:44 +0800

    更新camel测试代码和部分文档内容！

---
+++ src/main/java/com/camel/test/demo/document/CamelIntroduction04_txt.java
@@ -0,0 +1,11 @@
+package com.camel.test.demo.document;
+
+/**
+ * 粗糙翻译一下，后续调整，感兴趣同学，自行阅读英文原版《Camel in action》
+ *
+ * Created by keen.zhao on 2016/10/2.
+ */
+public class CamelIntroduction04_txt {
+
+
+}
