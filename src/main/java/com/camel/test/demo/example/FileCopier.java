801abe7813ce7130b8d4b42facbe2a0250b24141
zhaoyulong <1307232659@qq.com>
2016-10-01 21:25:10 +0800

    更新camel测试代码和部分文档内容！

--- src/main/java/com/camel/test/demo/example/FileCopier.java
+++ src/main/java/com/camel/test/demo/example/FileCopier.java
@@ -1,10 +1,42 @@
 package com.camel.test.demo.example;
 
+import java.io.*;
+
 /**
- *
+ * 文件拷贝的类例子
+ * <p>
  * Created by keen.zhao on 2016/10/1.
  */
 public class FileCopier {
 
+    public static void main(String[] args) throws IOException {
+        File inBoxDirectory = new File("data/inbox");
+        File outBoxDirectory = new File("data/outbox");
+        outBoxDirectory.mkdir();
 
+        File[] files = inBoxDirectory.listFiles();
+        for (File source : files) {
+            if (source.isFile()) {
+                File dest = new File(outBoxDirectory.getPath()
+                        + File.separator + source.getName());
+                copyFile(source, dest);
+            }
+        }
+    }
+
+    private static void copyFile(File source, File dest) throws IOException {
+
+        OutputStream out = new FileOutputStream(dest);
+        byte[] buffer = new byte[((int) source.length())];
+        InputStream in = new FileInputStream(source);
+
+        in.read(buffer); //从输入文件数据读入buffer
+
+        try {
+            out.write(buffer); //把buffer写入到输出文件
+        } finally {
+            out.close();
+            in.close();
+        }
+    }
 }
