ffc6158b4eb1c9bd136892b0ef5d1db648cbf0fa
zhaoyulong <1307232659@qq.com>
2016-10-03 00:52:15 +0800

    更新camel测试代码和部分文档内容！

--- src/main/java/com/camel/test/demo/document/c1/CamelIntroduction04_txt.java
+++ src/main/java/com/camel/test/demo/document/c1/CamelIntroduction04_txt.java
@@ -63,6 +63,59 @@ public class CamelIntroduction04_txt {
      *              确保消息正确路由的重活。
      *
      *           ★ Routes（路由）
+     *              Camel的Routes显然是一个核心抽象概念。定义一个路由的最简单的办法是当作一系列处理器。在消息应用程序
+     *              中使用路由器的原因有很多。出自客户端从服务器以及生产者从消费者的解耦的需要，routes能做下面这些：
+     *                    ■ 动态决定客户端将调用什么服务器
+     *                    ■ 提供一种灵活的方式来增加额外的处理
+     *                    ■ 允许客户端和服务器是独立开发
+     *                    ■ 允许服务器的客户端无需存根（使用Mock）来达到测试目的
+     *                    ■ 通过连接不同的系统做好一件事来促进更好的设计实践
+     *                    ■ 增强一些系统的特点和功能（如message brokers和ESBs）
+     *
+     *              Camel的路由有一个唯一的标识符用于日志、调试、监控以及启动和停止路由。路由也要有确切的一个消息
+     *              输入源，它们被有效地绑定到一个输入端点上（endpoint）。
+     *              为了定义一个路由，需要使用一个DSL。
+     *
+     *           ★ DOMAIN-SPECIFIC LANGUAGE (DSL) 领域特定语言
+     *              Camel定义一个DSL将processors和endpoint连接在一起形成路由，术语DSL用在这里有点随意。在Camel中
+     *              DSL意味着一个包含符合EIP术语的命名方法的优雅的java API。
+     *                 考虑这个例子：
+     *                      from("file:data/inbox")
+     *                          .filter().xpath("/order[not(@test)]")
+     *                          .to("jms:queue:order")
+     *                 这里在单一的java语句里，你定义了一个从文件端点消费文件的路由。消息然后路由到EIP过滤器，此过
+     *                 滤器使用一个XPath断言来测试消息是否是一个order测试，如果消息通过测试，它将被转发到JMS端点。
+     *                 过滤器测试失败消息将被删除。
+     *
+     *              Camel提供多种DSL语言，因此也可以使用Spring DSL来定义与上面相同的路由，像这样：
+     *                     <route>
+     *                         <from uri="file:data/inbox"/>
+     *                         <filter>
+     *                             <xpath>/order[not(@test)]</xpath>
+     *                             <to uri="jms:queue:order"/>
+     *                         </filter>
+     *                     </route>
+     *              DSLs为Camel用户创建应用提供了很好的抽象,尽管在底层，一个路由实际上是由一个processors图组成的。
+     *
+     *            ★ PROCESSOR（处理器）
+     *               处理器是Camel的一个核心概念，代表一个节点有使用、创建或修改传入的Exchange的能力。
+     *               在路由期间，exchange流是从一个processor到里一个processor的；这样，你可以把一个路由当做一张图
+     *               来理解，图中的每个节点都是特定的processor，图中的连线是连线是连接一个processor的输出到另一个
+     *               processor的输入。许多processor是EIPs的实现，但可以很容易实现自己定制的processor并插入到路由中。
+     *               那么这个processor图的exchanges是如何输入(get in)和输出(get out)的呢？要理解这个，我们需要看下
+     *               components和endpoint。
+     *
+     *            ★ COMPONENT（组件）
+     *               在Camel中，components是主要的扩展点。到目前为止，在Camel的生态系统中有超过80个（实际现在已经超过
+     *               150个了）components，功能范围从数据传输、DSLs、数据格式等等。你甚至可以为Camel创建自己的组件。
+     *               在第11章我们将讨论它。
+     *               从编程的观点看，components是相当简单的：关联一个在URI中使用的名字，充当一个端点工厂。例如，一个
+     *               FileComponent通过在uri中的file来指定，并且它创建了一个FileEndpoints。
+     *               endpoint也许是Camel中更基本的概念。
+     *
+     *            ★ ENDPOINT（端点）
+     *
+     *
      *
      *
      */
