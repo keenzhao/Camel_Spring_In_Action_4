ab17f05062ed28c07bd55b498a54ded5df077d8f
zhaoyulong <1307232659@qq.com>
2016-10-04 19:25:08 +0800

    CamelInAction第一章翻译完成。

--- src/main/java/com/camel/test/demo/document/c2/RoutingWithCamel01_txt.java
+++ src/main/java/com/camel/test/demo/document/c2/RoutingWithCamel01_txt.java
@@ -86,10 +86,76 @@ public class RoutingWithCamel01_txt {
      *       就是发送你从FTP服务器下载的订单到一个JMS队列。这个过程需要多一点的设置，但它仍然很容易。
      *
      * 2.2.2 发送到JMS队列（Sending to a JMS queue）
+     *       Camel为JMS实现提供者提供了广泛的连接支持，在第7章我们将涵盖所有的细节。但是现在，我们只要涉及足够你能完成Rider
+     *       汽车配件公司的第一个任务就可以了。回想一下，你需要从FTP服务器上下载订单并将其发送到JMS队列。
      *
+     *       ★ JMS是什么？（WHAT IS JMS?）
+     *          MS（java消息服务）是一个java的API，允许你创建、发送、接收、和阅读消息。它还规定，消息传递是异步的，
+     *          具有特定的可靠性元素，像有保证的和一次且仅一次投递。在java社区,JMS是事实上的消息传递解决方案。
+     *          在JMS中，消息的消费者和生产者彼此交谈是通过中间人 - 一个JMS目的地。如图2.4所示，一个JMS目的地可以
+     *          是一个队列或一个主题。队列是严格的点对点，每个消息只有一个消费者。主题用于发布/订阅体系，一个单一
+     *          的消息可能会被投递给许多消费者，如果它们已经订阅了这个主题。JMS也提供了ConnectionFactory，客户端
+     *          （像Camel）可以用它来创建一个到JMS提供者的连接。JMS提供者通常称为经纪人（brokers），因为他们在生
+     *          产者和消费者之间管理消息的沟通。
      *
+     *       ★ 如何配置Camel使用JMS提供者（HOW TO CONFIGURE CAMEL TO USE A JMS PROVIDER）
+     *          为了将Camel连接到一个特定的JMS提供者，你需要使用一个适当ConnectionFactory来配置Camel的JMS组件。
+     *          Apache ActiveMQ是一个最流行的开源JMS提供者，并且它是Camel团队用来测试JMS组件的主要JMS broker。
+     *          因此，在本书中我们用它来说明JMS概念。对于Apache ActiveMQ的更多信息，我们推荐由Bruce Snyder、
+     *          Dejan Bosanac和Rob Davies合著的《ActiveMQ in Action》这本书。
      *
+     *          因此在Apache ActiveMQ的案例中，你能创建一个指向运行ActiveMQ broker位置的ActiveMQConnectionFactory：
      *
+     *             ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+     *
+     *          vm://localhost这个URI表示在你该连接到一个内嵌在当前的JVM中的指定为"localhost"的正在运行的broker。
+     *          在ActiveMQ中，如果没有运行，vm传输连接器根据需要随时创建broker，所以对于快速测试JMS应用是非常好用的；
+     *          对于生产环境，建议您连接到已经运行的broker（代理）。此外，在生产环境中，我们推荐在连接到JMS broker时
+     *          使用连接池。详见第7章的这些替换配置的详细信息。
+     *
+     *          接下来，当你创建你的CamelContext，您可以添加JMS组件如下：
+     *
+     *             CamelContext context = new DefaultCamelContext();
+     *             context.addComponent("jms",JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+     *
+     *          JMS组件和ActiveMQ专用的连接工厂不是Camel的核心模块。为了使用这些，你需要添加一些依赖到你的基于Maven
+     *          的项目中去。对于普通的JMS组件，你要添加的是：
+     *              <dependency>
+     *                  <groupId>org.apache.camel</groupId>
+     *                  <artifactId>camel-jms</artifactId>
+     *                  <version>2.5.0</version>
+     *              </dependency>
+     *
+     *          连接工厂直接来自ActiveMQ，所以你需要以下依赖:
+     *              <dependency>
+     *                  <groupId>org.apache.activemq</groupId>
+     *                  <artifactId>activemq-core</artifactId>
+     *                  <version>5.3.2</version>
+     *              </dependency>
+     *
+     *          现在你已经配置JMS组件连接到一个真实的JMS的broker，是时候看看URIs如何能被用于指定目的地。
+     *
+     *       ★ 使用URIS指定目的地（USING URIS TO SPECIFY THE DESTINATION）
+     *          一旦JMS组件被配置好，你就可以在你的空闲时间开始发送和接收JMS消息。因为你正在使用的URIs，对配置而言
+     *          这是一个轻而易举的事。比方说，你想送一个JMS消息到命名为incomingOrders队列。在这种情况下URI将是：
+     *
+     *                jms:queue:incomingOrders
+     *
+     *          这是相当明显的。“JMS”前缀表明你使用之前配置的JMS组件，通过指定“queue”, JMS组件知道将消息发送到一个
+     *          名为incomingOrders的队列。你甚至可以省略队列限定符，因为默认的行为是发送到队列而不是一个主题。
+     *
+     *                注意：一些端点可能有一个令人生畏的端点URI属性列表。例如，MS组件约有60个选项，其中有许多是
+     *                只能用于特定的JMS场景。Camel总是试图提供内置的默认值以适合大多数情况，通过浏览联机Camel文档中
+     *                的组件页面你总是可以找到那些默认值。
+     *                JMS组件的讨论在这：http://camel.apache.org/jms.html
+     *
+     *           使用Camel的java DSL，通过使用关键字to你可以发送一条消息到incomingOrders，像这样：
+     *
+     *                ...to("jms:queue:incomingOrders")；
+     *
+     *           这可以解读为：发送到名为incomingOrders的JMS队列。
+     *
+     *           现在你知道了FTP和JMS与Camel通信的基本知识，你可以回到本章的路由主题和开始路由一些信息了！
      *
      。
      */
