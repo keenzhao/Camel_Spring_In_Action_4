53d99f59e2beb2cea957126bb89b84e2c9ad790f
zhaoyulong <1307232659@qq.com>
2016-10-13 04:58:10 +0800

    第二章翻译完成，第三章开始翻译。

---
+++ src/main/java/com/camel/test/demo/document/c3/TransformingDataWithCamel01_txt.java
@@ -0,0 +1,26 @@
+package com.camel.test.demo.document.c3;
+
+/**
+ * 粗糙翻译一下，后续调整，感兴趣同学，自行阅读英文原版《Camel in action》
+ *
+ * Created by keen.zhao on 2016/10/13.
+ */
+public class TransformingDataWithCamel01_txt {
+
+    /**
+     * 本章介绍
+     *  ■ 利用EIPs和java进行转换数据
+     *  ■ 转换XML数据
+     *  ■ 使用众所周知的数据格式转换
+     *  ■ 编写自己的数据格式转换
+     *  ■ 了解Camel类型转换机制
+     *
+     *
+     * 3.1 数据转换概述（Data transformation overview）
+     *
+     *
+     *
+     *
+     *
+     */
+}
